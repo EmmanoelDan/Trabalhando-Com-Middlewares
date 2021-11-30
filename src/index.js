afacce84a573e1d09cd0cbeb481bd8f4ad63fe84
EmmanoelDan <emmmanuel2011.07@gmail.com>
2021-11-30 15:30:42 -0300

    Segundo middleware feito

--- src/index.js
+++ src/index.js
@@ -27,11 +27,20 @@ function checksExistsUserAccount(request, response, next) {
 
 function checksCreateTodosUserAvailability(request, response, next) {
   // Complete aqui
+  const {user} = request;
+  if (!user.pro && user.todos.length < 10){
+    return next();
+  } else if (user.pro){
+    return next();
+  }else {
+    return response.status(403).json({error : 'limit reached'});
+  }
   
 }
 
 function checksTodoExists(request, response, next) {
   // Complete aqui
+  
 }
 
 function findUserById(request, response, next) {
